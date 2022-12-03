24c5ad60f72a763e357d314e8aa7de2754f77cc0
Ondra <Ondra@ONDRA-PC>
2022-12-03 08:57:59 +0100

    3

---
+++ SolverAOC2022_03/Data.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SolverAOC2022_03
+{
+  internal class Data
+  {
+    private List<string> items = new List<string>();
+
+    public Data(string input)
+    {
+      using (StringReader sr = new StringReader(input))
+      {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+          items.Add(line);
+        }
+      }
+    }
+
+    internal int Solve1()
+    {
+      int val = 0;
+      foreach(string item in items)
+      {
+        string first = item.Substring(0, item.Length / 2);
+        string second = item.Substring(item.Length / 2);
+        foreach(char c in first)
+        {
+          if (second.Contains(c))
+          {
+            if(c >= 'a' && c<= 'z')
+            {
+              val += c - 'a' + 1;
+            }
+            if (c >= 'A' && c <= 'Z')
+            {
+              val += c - 'A' + 27;
+            }
+            break;
+          }
+        }
+
+      }
+      return val;
+    }
+
+    internal int Solve2()
+    {
+      int val = 0;
+      for(int i = 0; i < items.Count; i+=3)
+      {
+        foreach (char c in items[i])
+        {
+          if (items[i + 1].Contains(c) && items[i + 2].Contains(c))
+          {
+            if (c >= 'a' && c <= 'z')
+            {
+              val += c - 'a' + 1;
+            }
+            if (c >= 'A' && c <= 'Z')
+            {
+              val += c - 'A' + 27;
+            }
+            break;
+          }
+        }
+        
+      }
+      return val;
+    }
+  }
+}
--- SolverAOC2022_03/Program.cs
+++ SolverAOC2022_03/Program.cs
@@ -10,18 +10,25 @@ namespace SolverAOC2022_03
   public class Program : BaseAdventSolver, IAdventSolver
   {
 
-    public override string SolverName => "SolverAOC2022_03"/*TODO: Task Name*/;
+    public override string SolverName => "Day 3: Rucksack Reorganization"/*TODO: Task Name*/;
 
     public override string InputsFolderName => "SolverAOC2022_03";
 
     public override string SolveTask1(string InputData)
     {
-      throw new NotImplementedException();
+
+      Data d = new Data(InputData);
+      int res = d.Solve1();
+
+      return res.ToString();
     }
 
     public override string SolveTask2(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve2();
+
+      return res.ToString();
     }
   }
 }
