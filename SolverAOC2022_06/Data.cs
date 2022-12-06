b3596f453fd78941dcad6d0259ae191821db7e9e
Ondřej Brožek <obrozek@centrum.cz>
2022-12-06 06:54:31 +0100

    6

---
+++ SolverAOC2022_06/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SolverAOC2022_06
+{
+  internal class Data
+  {
+    private string inputData;
+
+    public Data(string inputData)
+    {
+      this.inputData = inputData;
+    }
+
+    internal int Solve1()
+    {
+      return Find(4);
+    }
+
+    private int Find(int charCnt)
+    {
+      for (int i = 0; i < inputData.Length - charCnt; i++)
+      {
+        string substr = inputData.Substring(i, charCnt);
+        if (substr.GroupBy(x => x).Count() == charCnt)
+        {
+          return i + charCnt;
+        }
+      }
+      return -1;
+    }
+
+    internal int Solve2()
+    {
+      return Find(14);
+    }
+  }
+}
--- SolverAOC2022_06/Program.cs
+++ SolverAOC2022_06/Program.cs
@@ -10,18 +10,22 @@ namespace SolverAOC2022_06
   public class Program : BaseAdventSolver, IAdventSolver
   {
 
-    public override string SolverName => "SolverAOC2022_06"/*TODO: Task Name*/;
+    public override string SolverName => "Day 6: Tuning Trouble"/*TODO: Task Name*/;
 
     public override string InputsFolderName => "SolverAOC2022_06";
 
     public override string SolveTask1(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve1();
+      return res.ToString();
     }
 
     public override string SolveTask2(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve2();
+      return res.ToString();
     }
   }
 }
