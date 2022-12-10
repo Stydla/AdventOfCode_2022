4fc4824e6a5f97017a526457f7d99d52e6bc42ab
Ondra <Ondra@ONDRA-PC>
2022-12-10 07:10:27 +0100

    10

---
+++ SolverAOC2022_10/Data.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2022_10
+{
+  internal class Data
+  {
+    private string inputData;
+
+    private List<int> RegisterX = new List<int>();
+
+    public Data(string inputData)
+    {
+      this.inputData = inputData;
+
+      using (StringReader sr = new StringReader(inputData))
+      {
+        RegisterX.Add(1);
+        int lastVal = 0;
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+          if(line.StartsWith("addx"))
+          {
+            
+            RegisterX.Add(RegisterX.Last() + lastVal);
+            RegisterX.Add(RegisterX.Last());
+
+            lastVal = int.Parse(line.Substring(5));
+          } else
+          {
+            RegisterX.Add(RegisterX.Last() + lastVal);
+            lastVal = 0;
+          }
+        }
+      }
+    }
+
+    internal int Solve1()
+    {
+      int sum = 0;
+      for(int i = 0; i < 6; i++)
+      {
+        int index = 20 + 40 * i;
+        sum += RegisterX[index] * index;
+      }
+      return sum;
+    }
+
+    internal string Solve2()
+    {
+      StringBuilder sb = new StringBuilder();
+      int lineLength = 40;
+      for(int i = 0; i < 240; i++) 
+      {
+        if (i % lineLength == 0 && i != 0)
+        {
+          sb.AppendLine();
+        }
+
+        int index = i % lineLength;
+        int val = RegisterX[i+1];
+        if (val - 1 <= index && val + 1 >= index)
+        {
+          sb.Append('#');
+        } else
+        {
+          sb.Append('.');
+        }
+
+        
+      }
+      return sb.ToString();
+    }
+  }
+}
--- SolverAOC2022_10/Program.cs
+++ SolverAOC2022_10/Program.cs
@@ -10,18 +10,22 @@ namespace SolverAOC2022_10
   public class Program : BaseAdventSolver, IAdventSolver
   {
 
-    public override string SolverName => "SolverAOC2022_10"/*TODO: Task Name*/;
+    public override string SolverName => "Day 10: Cathode-Ray Tube"/*TODO: Task Name*/;
 
     public override string InputsFolderName => "SolverAOC2022_10";
 
     public override string SolveTask1(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve1();
+      return res.ToString();
     }
 
     public override string SolveTask2(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      string res = d.Solve2();
+      return res;
     }
   }
 }
